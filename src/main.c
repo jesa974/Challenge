836b64f118d31bf476433427b279a9c4b6bf76a9
Matthieu Prezut <jesa974@gmail.com>
2022-04-10 21:42:12 +0200

    Removing unue

--- src/main.c
+++ src/main.c
@@ -32,36 +32,12 @@ int main(void)
 
 		flow_array = gather_flows(file_content, &max);
 
-		for (int i = 0; i < max; i++) {
-			printf("%s:%d -> %s:%d\n", flow_array[i].src_ip, flow_array[i].src_port, flow_array[i].dst_ip, flow_array[i].dst_port);
-			printf("NB: %d\n", flow_array[i].nb_packet);
-			for (int j = 0; j < flow_array[i].nb_packet; j++) {
-				printf("Seq %d : %d\n", j, flow_array[i].seq[j]);
-			}
-		}
-
-		printf("NB flow: %d\n", max);
-
 		struct counter* length_array = (struct counter*)malloc(sizeof(struct counter) * max);
 		
 		length_array = count_len(flow_array, max);
 
-		printf("Classic sort\n");
-		for (int i = 0; i < max; i++) {
-			printf("%s:%d -> %s:%d = %d\n", flow_array[length_array[i].index].src_ip, flow_array[length_array[i].index].src_port, flow_array[length_array[i].index].dst_ip, flow_array[length_array[i].index].dst_port, length_array[i].length);
-		}
-		printf("\n\n\n");
-
 		printf("ASC sort\n");
 		sort_by_len(length_array, max, "asc");
-
-		for (int i = 0; i < max; i++) {
-			printf("%s:%d -> %s:%d = %d\n", flow_array[length_array[i].index].src_ip, flow_array[length_array[i].index].src_port, flow_array[length_array[i].index].dst_ip, flow_array[length_array[i].index].dst_port, length_array[i].length);
-		}
-		printf("\n\n\n");
-
-		printf("DESC sort\n");
-		sort_by_len(length_array, max, "desc");
 
 		for (int i = 0; i < max; i++) {
 			printf("%s:%d -> %s:%d = %d\n", flow_array[length_array[i].index].src_ip, flow_array[length_array[i].index].src_port, flow_array[length_array[i].index].dst_ip, flow_array[length_array[i].index].dst_port, length_array[i].length);
